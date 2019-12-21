0bf98213e14fbc00386db63705604b32f4b56fa1
ProgammingStudent1 <vohaiphong11@gmail.com>
2019-12-21 10:03:41 +0700

    qưewqe

--- endProject/script.js
+++ endProject/script.js
@@ -1,6 +1,6 @@
-        var save_src = "";
+       var save_src = "";
         var save_id = "";
-        var check = 0 ;
+        var check = 0;
         var count = 0;
         var turn = 1;
         var boardChess=[
@@ -33,7 +33,7 @@
                     document.getElementById("div"+(x+1)+"_"+y).style.backgroundColor = "green";
                     document.getElementById("div"+(x+2)+"_"+y).style.backgroundColor = "green";
                     count++
-                }
+                } 
                 else if(x+1<8 && boardChess[x+1][y]==0){
                     document.getElementById("div"+(x+1)+"_"+y).style.backgroundColor = "green";
                     count += 1;
@@ -348,7 +348,7 @@
                 }
             }
             y = y_save
-             x = x_save+1;
+            x = x_save+1;
             y = y_save+1;
             while(x>=0 && x<8 && y>=0 && y<8){
                 if(boardChess[x][y]==0){
@@ -556,103 +556,103 @@
             document.getElementById('promotion').style.display="none";
         }
     }
-            function test(id){
-                var id_1 = id.slice(3,id.length);
-                var s = id_1.split('_');
-                var x = Number(s[0]);
-                var y = Number(s[1]);
-                if(check == 0){
-                if(boardChess[x][y]==0) return;
-                if(turn*boardChess[x][y]>0){
-                    return
-                }
-                if(boardChess[x][y]==1){
-                    save_id = 'div'+x+"_"+y;
-                    save_src = document.getElementById('div'+x+y).src;
-                    bPawn(x,y);
-                }
-                if(boardChess[x][y]==-1){
-                    save_id = 'div'+x+"_"+y;
-                    save_src = document.getElementById('div'+x+y).src; 
-                    wPawn(x,y);
-                }
-                if(boardChess[x][y]==4 || boardChess[x][y]==-4){
-                        save_id = 'div'+x+"_"+y;
-                        save_src = document.getElementById('div'+x+y).src;
-                        bishop(x,y);
-                }
-                if(boardChess[x][y]==2 || boardChess[x][y]==-2){
-                    save_id = 'div'+x+"_"+y;
-                    save_src = document.getElementById('div'+x+y).src;
-                    rook(x,y);
-                }
-                if(boardChess[x][y]==5 || boardChess[x][y]==-5){
-                    save_id = 'div'+x+"_"+y;
-                    save_src = document.getElementById('div'+x+y).src;
-                    queen(x,y)
-                }
-                if(boardChess[x][y]==6 || boardChess[x][y]==-6){
-                    save_id = 'div'+x+"_"+y;
-                    save_src = document.getElementById('div'+x+y).src;
-                    king(x,y)
-                }
-                if(boardChess[x][y]==3 || boardChess[x][y]==-3){
-                    save_id = 'div'+x+"_"+y;
-                    save_src = document.getElementById('div'+x+y).src;
-                    knight(x,y)
-                }
-                if(count !=0){
-                    count = 0;
-                    check = 1;
-                }
-                }
-            else if(check == 1){
-                save_id = save_id.slice(3,save_id.length);
-                var s_1 = save_id.split("_");
-                var x_1 = Number(s_1[0]);
-                var y_1 = Number(s_1[1]);
-                var ss = document.getElementById(id);
-                var img_next = document.getElementById('div'+x+y);
-                var img_save = document.getElementById('div'+x_1+y_1);
-                if(ss.style.backgroundColor == "green"){
-                    return_backgroundcolor();
-                    img_next.src = save_src
-                    img_save.src = "alt";
-                    if(boardChess[x][y]){
-                        eat(x,y)
-                    }
-                    boardChess[x][y] = boardChess[x_1][y_1];
-                    boardChess[x_1][y_1] = 0;
-                    check = 0;
-                    turn = 0-turn
-                    if (boardChess[x][y]==1 && boardChess[x-1][y]==-1 && checkPawn==true) {
-                        boardChess[x-1][y]==0
-                        document.getElementById('div'+(x-1)+y).src='alt'
-                    }
-                    else if (boardChess[x][y]==-1 && boardChess[x+1][y]==1 && checkPawn==true) {
-                        boardChess[x+1][y]==0
-                        document.getElementById('div'+(x+1)+y).src='alt'
-                    }
-                    checkPawn=false
-                    if (checkPawnClick) {
-                        if (Math.abs(x-x_1)==2 && y==y_1) {
-                            checkPawn=true
-                            pawnX=x
-                            pawnY=y
-                        }
-                    }
-                }
-                else{
-                    checkPawn = false
-                    return_backgroundcolor();
-                    check = 0;
-                    return;
-                }
+    function test(id){
+        var id_1 = id.slice(3,id.length);
+        var s = id_1.split('_');
+        var x = Number(s[0]);
+        var y = Number(s[1]);
+        if(check == 0){
+        if(boardChess[x][y]==0) return;
+        if(turn*boardChess[x][y]>0){
+            return
+        }
+        if(boardChess[x][y]==1){
+            save_id = 'div'+x+"_"+y;
+            save_src = document.getElementById('div'+x+y).src;
+            bPawn(x,y);
+        }
+        if(boardChess[x][y]==-1){
+            save_id = 'div'+x+"_"+y;
+            save_src = document.getElementById('div'+x+y).src; 
+            wPawn(x,y);
+        }
+        if(boardChess[x][y]==4 || boardChess[x][y]==-4){
+                save_id = 'div'+x+"_"+y;
+                save_src = document.getElementById('div'+x+y).src;
+                bishop(x,y);
+        }
+        if(boardChess[x][y]==2 || boardChess[x][y]==-2){
+            save_id = 'div'+x+"_"+y;
+            save_src = document.getElementById('div'+x+y).src;
+            rook(x,y);
+        }
+        if(boardChess[x][y]==5 || boardChess[x][y]==-5){
+            save_id = 'div'+x+"_"+y;
+            save_src = document.getElementById('div'+x+y).src;
+            queen(x,y)
+        }
+        if(boardChess[x][y]==6 || boardChess[x][y]==-6){
+            save_id = 'div'+x+"_"+y;
+            save_src = document.getElementById('div'+x+y).src;
+            king(x,y)
+        }
+        if(boardChess[x][y]==3 || boardChess[x][y]==-3){
+            save_id = 'div'+x+"_"+y;
+            save_src = document.getElementById('div'+x+y).src;
+            knight(x,y)
+        }
+        if(count !=0){
+            count = 0;
+            check = 1;
+        }
+        }
+    else if(check == 1){
+        save_id = save_id.slice(3,save_id.length);
+        var s_1 = save_id.split("_");
+        var x_1 = Number(s_1[0]);
+        var y_1 = Number(s_1[1]);
+        var ss = document.getElementById(id);
+        var img_next = document.getElementById('div'+x+y);
+        var img_save = document.getElementById('div'+x_1+y_1);
+        if(ss.style.backgroundColor == "green"){
+            return_backgroundcolor();
+            img_next.src = save_src
+            img_save.src = "alt";
+            if(boardChess[x][y]){
+                eat(x,y)
             }
-            if(turn>0){
-                document.getElementById("turnDisplay").innerHTML = "White's turn";
+            boardChess[x][y] = boardChess[x_1][y_1];
+            boardChess[x_1][y_1] = 0;
+            check = 0;
+            turn = 0-turn
+            if (boardChess[x][y]==1 && boardChess[x-1][y]==-1 && checkPawn==true) {
+                boardChess[x-1][y]==0
+                document.getElementById('div'+(x-1)+y).src='alt'
             }
-            else{
-                document.getElementById("turnDisplay").innerHTML = "Black's turn";
+            else if (boardChess[x][y]==-1 && boardChess[x+1][y]==1 && checkPawn==true) {
+                boardChess[x+1][y]==0
+                document.getElementById('div'+(x+1)+y).src='alt'
+            }
+            checkPawn=false
+            if (checkPawnClick) {
+                if (Math.abs(x-x_1)==2 && y==y_1) {
+                    checkPawn=true
+                    pawnX=x
+                    pawnY=y
+                }
             }
         }
+        else{
+            checkPawn = false
+            return_backgroundcolor();
+            check = 0;
+            return;
+        }
+    }
+    if(turn>0){
+        document.getElementById("turnDisplay").innerHTML = "White's turn";
+    }
+    else{
+        document.getElementById("turnDisplay").innerHTML = "Black's turn";
+    }
+}

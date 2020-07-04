1e9beda64ece949d516eef78d8e02bc6ceeb30f6
ProgammingStudent1 <vohaiphong11@gmail.com>
2020-07-04 20:21:49 +0700

    Update post.js

--- chat/post/post.js
+++ chat/post/post.js
@@ -1,13 +1,21 @@
-let imageSelector = document.querySelector("#image-selector")
-let imagePreviewer = document.querySelector("#image-preview")
-function onImageSelected(){
-    if (imageSelector.files && imageSelector.files[0]) {
-        var reader = new FileReader();
+let imageSelector = document.querySelector("#image-selector");
+let imagePreviewer = document.querySelector("#image-preview");
+function onImageSelected() {
+  if (imageSelector.files && imageSelector.files[0]) {
+    const image = imageSelector.files[0];
+    var reader = new FileReader();
 
-        reader.onload = function (e) {
-           imagePreviewer.src = e.target.result
-        };
-
-        reader.readAsDataURL(imageSelector.files[0]);
-    }
+    reader.onload = function (e) {
+      imagePreviewer.src = e.target.result;
+    };
+    upload(image);
+    reader.readAsDataURL(imageSelector.files[0]);
+  }
+}
+function upload(image){
+    let storageRef = firebase.storage().ref()
+    let imageRef = storageRef.child(image.name)
+    imageRef.put(image).then(function (snapshot) {
+    console.log('uploaded a blob or file!')
+    })
 }

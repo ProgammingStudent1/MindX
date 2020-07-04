bdcff235aa6774574bf945462da2533b24050ffb
ProgammingStudent1 <vohaiphong11@gmail.com>
2020-07-04 21:28:43 +0700

    aa

--- chat/post/post.js
+++ chat/post/post.js
@@ -12,10 +12,38 @@ function onImageSelected() {
     reader.readAsDataURL(imageSelector.files[0]);
   }
 }
-function upload(image){
-    let storageRef = firebase.storage().ref()
-    let imageRef = storageRef.child(image.name)
-    imageRef.put(image).then(function (snapshot) {
-    console.log('uploaded a blob or file!')
+function upload(image) {
+  let storageRef = firebase.storage().ref();
+  let imageRef = storageRef.child(image.name);
+  let task = imageRef.put(image);
+
+  task.on(
+    "state_changed",
+    function (snapshot) {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("upload is" + progress + "% done");
+    },
+    function (error) {
+      console.log(error);
+    },
+    function () {
+      task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        console.log("file available at", downloadURL);
+      });
+    }
+  );
+}
+
+function createPost(){
+    db.collection('post').doc().set({
+        avatar:
+        'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/cat-home-441939.jpg?h=83a2eac3&itok=RHVSjYLN',
+        name: 'Phong',
+        userId: "AKdArdZjNkB9bp3AVvkE",
+        imageURL: imageURL,
+        caption: captionInput.value,
+        timestamp: new Date()
+
+
     })
 }
